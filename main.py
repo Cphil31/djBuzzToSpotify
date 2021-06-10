3dbc1e21a28d9ce6ad8064f453996a0f3be9e58e
Cphil31 <cecilephilippe31@gmail.com>
2021-06-10 19:54:11 +0200

    go to html page

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-"""
--récuperer la liste qui m'interresses  sur dj buzz (scrapping)
--récuperer l'adresse de spotify + le nom et le prénom récupéré
--ouvrir le lien avec google chrome dans un nouvel onglet si possible
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+
+url ='https://www.djbuzz.com/mycharts/index.asp'
+
+response = requests.get(url)
+# récuperer le code html de la page
+#on vas le passer dans la fonction Beautifulsoup
+if response.ok:
+    #print(response.text)
+    #ne pas oublier de le parser en 2nd argument
+    soup = BeautifulSoup(response.text,"html.parser")
+
+    #je recupère un tableau
+    alltitle = soup.findAll(class_="title")
+
+    spotify = webdriver.Chrome(executable_path="chromedriver.exe")
+
+    for title in alltitle:
+        print(title.text)
+        spotify.get("https://open.spotify.com/search/"+str(title.text)).send_keys(Keys.COMMAND + 't')
 
 
 
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
