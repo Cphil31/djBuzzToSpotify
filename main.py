10dc9d90ac9e813a44d1d23df63304b36ca45cfc
Cphil31 <cecilephilippe31@gmail.com>
2021-06-10 15:06:17 +0200

    ecriture des instructions

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+"""
+-récuperer la liste qui m'interresses  sur dj buzz (scrapping)
+-récuperer l'adresse de spotify + le nom et le prénom récupéré
+-ouvrir le lien avec google chrome dans un nouvel onglet si possible
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
